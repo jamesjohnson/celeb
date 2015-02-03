ca552b649e47b0a29cabdb6671edbc7038626417
James Johnson <jamesjohnson11@gmail.com>
2015-02-02 21:17:06 -0500

    added updates

--- celeb/heroku.py
+++ celeb/heroku.py
@@ -6,9 +6,11 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'celeb',
-        'USER': 'celeb',
-        'PASSWORD': 'celeb123',
+        'NAME': 'instagram',
+        'USER': 'instagram',
+        'PASSWORD': 'instagram',
+        'HOST': 'instagram-1.cykvexeo9jm1.us-east-1.rds.amazonaws.com',
+        'PORT': 5432
     }
 }
 
